d811bedbe6ecf4ce80f3c79447db4d78da738294
HosseinDakhili <hosseindakhili94@gmail.com>
2025-08-05 16:37:37 +0330

    feat: add initial components and routing setup for the application

--- order_client/src/routes/index.jsx
+++ order_client/src/routes/index.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout";
+import Home from "../pages/Home/Home";
+import Foods from "../pages/Foods/Foods";
+import FoodDetails from "../pages/FoodDetails/FoodDetails";
+import Cart from "../pages/Cart/Cart";
+import Public from '../Routes/Authentication/Public'; 
+import Auth from "../pages/Auth/Auth";
+import NotFound from "../pages/NotFound/NotFound";
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Layout />,
+        children:[
+            {
 
-export default function Routes() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+                index:true,
+                element:<Home/>
+            },
+            {
+                path:'/foods/:categoryId/:categoryName',
+                element:<Foods/>
+            },
+            {
+                path:'/food-details/:id/:name',
+                element:<FoodDetails/>
+            },
+            {
+                path:'/cart',
+                element:<Public/>,
+                children:[
+                    {
+                        index:true,
+                        element:<Cart/>
+                    }
+                ]
+            },
+            {
+                path:'/auth',
+                element:<Public/>,
+                children:[
+                    {
+                        index:true,
+                        element:<Auth/>
+                    }
+                ]
+            }
+        ]
+
+    },
+    {
+        path:'*',
+        element:<NotFound/>
+    }
+])
+export default router
