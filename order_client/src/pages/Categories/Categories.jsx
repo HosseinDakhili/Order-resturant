0c14cab930ecd0fb605e2aeec6ee9c4b308e98c4
HosseinDakhili <hosseindakhili94@gmail.com>
2025-08-06 15:15:29 +0330

    feat: add react-helmet for managing document head; update Categories component to use Helmet for dynamic title

--- order_client/src/pages/Categories/Categories.jsx
+++ order_client/src/pages/Categories/Categories.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import assets from "../../assets";
 import fetchData from "../../Utils/fetchData";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 export default function Categories() {
   const [categories, setCategories] = useState([]);
@@ -10,34 +11,43 @@ export default function Categories() {
       const response = await fetchData("categories?populate=*");
       setCategories(response?.data);
     })();
+    document.title = 'دسته بندی ها'
   }, []);
   const navigate = useNavigate();
   const categoryItems = categories?.map((c) => {
     return (
-      <div
-        key={c?.id}
-        onClick={() =>
-          navigate(`/foods/${c?.id}/${c?.name.replaceAll(" ", "-")}`)
-        }
-        className="flex flex-col justify-center items-center gap-2"
-      >
-        <div className="w-[150px] h-[150px] rounded-full overflow-hidden ">
-          <img
-            className="w-full h-full "
-            src={
-              import.meta.env.VITE_BASE_FILE + c?.img?.formats?.thumbnail?.url
-            }
-            alt={c?.name}
-          />
+      <>
+        <div
+          key={c?.id}
+          onClick={() =>
+            navigate(`/foods/${c?.id}/${c?.name.replaceAll(" ", "-")}`)
+          }
+          className="flex flex-col justify-center items-center gap-2"
+        >
+          <div className="w-[150px] h-[150px] rounded-full overflow-hidden ">
+            <img
+              className="w-full h-full "
+              src={
+                import.meta.env.VITE_BASE_FILE + c?.img?.formats?.thumbnail?.url
+              }
+              alt={c?.name}
+            />
+          </div>
+          <h6 className="font-bold text-lg">{c?.name}</h6>
         </div>
-        <h6 className="font-bold text-lg">{c?.name}</h6>
-      </div>
+      </>
     );
   });
 
   return (
-    <section className="my-12 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-9 flex-wrap  ">
-      {categoryItems}
-    </section>
+    <>
+      {/* <head>
+        <title>دسته بندی ها</title>
+      </head> */}
+
+      <section className="my-12 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-9 flex-wrap  ">
+        {categoryItems}
+      </section>
+    </>
   );
 }
