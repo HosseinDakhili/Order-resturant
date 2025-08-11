1866980a883f678826cb66c8cdcd1ba366a5a432
HosseinDakhili <hosseindakhili94@gmail.com>
2025-08-11 16:35:54 +0330

    feat: integrate redux-persist for state persistence and update store configuration

--- order_client/src/store/index.js
+++ order_client/src/store/index.js
@@ -1,14 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authSliceReducer from './Slices/AuthSlice'
-import cartSliceReducer from './Slices/CartSlice'
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import authSliceReducer from './Slices/AuthSlice';
+import cartSliceReducer from './Slices/CartSlice';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth', 'cart'],
+};
+
+const rootReducer = combineReducers({
+  auth: authSliceReducer,
+  cart: cartSliceReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    reducer:{
-        auth:authSliceReducer,
-        cart:cartSliceReducer
-    }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-}
-)
+export const persistor = persistStore(store);
 
-export default store
+export default store;
